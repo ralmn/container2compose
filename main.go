abddf2d675dc528d9339fef8b63e5bd549884a67
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2018-04-02 15:33:12 +0200

    Add volumes, port and commands

--- main.go
+++ main.go
@@ -6,43 +6,42 @@ import (
 	"os"
 	"docker.io/go-docker"
 	"context"
-
 	"docker.io/go-docker/api/types"
 	"fmt"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"reflect"
+	"path"
 )
 
-type Volume struct {
-	Type string
-	Source string
-	Target string
-}
-
 type Container struct {
-	Image string `yaml:"image"`
-	Volumes []Volume `yaml:"volumes"`
-	Args []string `yaml:"args"`
-
+	Image    string   `yaml:"image"`
+	Volumes  []string `yaml:"volumes,omitempty"`
+	Ports    []string `yaml:"ports,omitempty"`
+	Commands []string `yaml:"commands,omitempty"`
 }
 
+var output string
 
-func main(){
+func main() {
 
-	flag.Usage = func(){
+	flag.Usage = func() {
 		logrus.Info("Usage of container2compose : ")
+		logrus.Infof("\t%s -output/-o docker-compose.yml : Out file", os.Args[0])
 		logrus.Infof("\t%s <container name/id>", os.Args[0])
 	}
 
+	flag.StringVar(&output, "output", "docker-compose.yml", "output of file")
+
 	flag.Parse()
-	if flag.NArg() == 0{
+	if flag.NArg() == 0 {
 		flag.Usage()
 		return
 	}
 
-
 	toWrite := make(map[string]interface{})
 
 	toWrite["version"] = "3"
-
 
 	for _, containerId := range flag.Args() {
 
@@ -60,44 +59,66 @@ func main(){
 			continue
 		}
 
-		toWrite[containerData.Name] = CreateContainer(dockerCli, containerData)
+		containerName := path.Base(containerData.Name)
 
+		toWrite[containerName] = CreateContainer(dockerCli, containerData)
 
 	}
 
-	toWrite["fin"] = 0
-
 	logrus.Infof("res : %+v", toWrite)
 
-
-
+	if data, err := yaml.Marshal(&toWrite); err == nil {
+		ioutil.WriteFile(output, data, 0644)
+	} else {
+		logrus.Errorf("Failed to save on file %s", output)
+		panic(err)
+	}
 
 }
 
-func CreateContainer(dockerCli docker.APIClient, containerData types.ContainerJSON) (Container){
+func CreateContainer(dockerCli docker.APIClient, containerData types.ContainerJSON) (Container) {
 	image := containerData.Image
+
+	var volumes []string
+	var ports []string
+	var commands []string
+
+	commands = containerData.Config.Cmd
 
 	if imageData, _, err := dockerCli.ImageInspectWithRaw(context.Background(), containerData.Image); err == nil {
 		tags := imageData.RepoTags
 		if len(tags) > 0 {
 			image = tags[0]
 		}
-
+		if reflect.DeepEqual(imageData.Config.Cmd, containerData.Config.Cmd) {
+			commands = commands[:0]
+		}
 	}
-
-	var volumes []string
-
 
 	for _, mount := range containerData.Mounts {
 		rw := ""
 		if !mount.RW {
 			rw = ":r"
 		}
-		volumes = append(volumes, fmt.Sprintf("%s:%s%s", mount.Source, mount.Destination, rw) )
+		volumes = append(volumes, fmt.Sprintf("%s:%s%s", mount.Source, mount.Destination, rw))
+	}
+
+	for port, portBind := range containerData.NetworkSettings.Ports {
+		for _, bind := range portBind {
+			hostIp := ""
+			if bind.HostIP != "0.0.0.0" {
+				hostIp = bind.HostIP
+			}
+			portStr := fmt.Sprintf("%s%s:%s", hostIp, bind.HostPort, port.Port())
+			ports = append(ports, portStr)
+		}
 	}
 
 	return Container{
-		Image: image,
+		Image:    image,
+		Volumes:  volumes,
+		Ports:    ports,
+		Commands: commands,
 	}
 
 }
